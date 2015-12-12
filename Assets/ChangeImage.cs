f6653a0402300263f77c7dc6df698c898e61107a
Taylor Gandolfi <tsg10@miami.edu>
2015-12-11 22:44:12 -0500

    Unity VR App
    Unity VR App for 360 Storytelling final

---
+++ Assets/ChangeImage.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ChangeImage : MonoBehaviour {
+	public Renderer skyboxTop;
+	public Renderer skyboxBottom;
+	public Material skyTop;
+	public Material skyBottom;
+
+	private int currentSky = 0;
+	
+	void Start () {
+		
+	}
+	
+	public void clickerChange() {
+		switch (currentSky) {
+		case 0:
+			skyboxTop.material = skyTop;
+			skyboxBottom.material = skyBottom;
+			currentSky++;
+			break;
+		case 1:
+			skyboxTop.material = skyTop;
+			skyboxBottom.material = skyBottom;
+			currentSky = 0;
+			break;
+		}
+	}
+	public void stareChange() {
+		GetComponent<ParticleSystem>().Emit(10); 
+	}
+}
